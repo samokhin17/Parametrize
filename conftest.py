9ea3028840cd9d52d62ae4c99ab33361aec0bc3f
samokhin17 <88451055+samokhin17@users.noreply.github.com>
2022-05-05 15:33:26 +0300

    Я сделал параметризацию

--- conftest.py
+++ conftest.py
@@ -1,27 +1,29 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default='Chrome',
+    parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default=None,
-                     help="Choose language: ru, ung etc")
+                     help="Choose language: ru or eng")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    user_language = request.config.getoption("language")
     browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("browser_name")
+    browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
-        print("\nstart firefox browser for test..")
         browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
--- test_items.py
+++ test_items.py
@@ -1,9 +1,9 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-import pytest
 
 link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
     browser.find_element_by_css_selector("#login_link")
+
+    browser.find_element(By.TAG_NAME, 'button[value="Добавить в корзину"]')
